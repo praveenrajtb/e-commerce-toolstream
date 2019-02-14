7766c5fa0dd9bf65319f7c6b6f7b08d32e6d7005
praveenrajtb <44532643+praveenrajtb@users.noreply.github.com>
2019-02-14 15:08:45 +0530

    Update tools.py

--- tools.py
+++ tools.py
@@ -7,11 +7,14 @@ class ToolsSpider(scrapy.Spider):
 	start_urls = ['http://toolstream.com/']
 	
 	def parse(self, response):
+# Brands are list of brands and categories are the categories associated with each brands		
 		brands = response.xpath('//*[@class="leftNavCategoriesNodeName"]')
 		categories = response.xpath('//*[@class="leftNavCategoriesNodePopupColumn"]')
+# Select each brand and the category list associted with that brand
 		for brand, category in zip(brands, categories):
 			cat = category.xpath('.//a/@href').extract()
 			br = brand.xpath('.//text()').extract_first()
+# Select all the categories associated with each brand and yield			
 			for c in cat:
 				yield{'Brands': br, 'Categories_URL': response.urljoin(c)}
 		
